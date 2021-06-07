c221b33b6de7e68d63f5e9a531cd75ceb376aeb2
Rick Ellis <riccello@yahoo.com>
2021-06-07 09:13:39 -0500

    Updates

---
+++ precache-manifest.3179e37b67ff6b696fcbf5abca8cc423.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abbeb410331dd84fb4dfa78f385a5430",
+    "url": "/sample-gator/index.html"
+  },
+  {
+    "revision": "d39037445e6efb4afaf1",
+    "url": "/sample-gator/static/css/main.0cabef19.chunk.css"
+  },
+  {
+    "revision": "8ba5f44b31988490e50b",
+    "url": "/sample-gator/static/js/2.4030d5c0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/sample-gator/static/js/2.4030d5c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d39037445e6efb4afaf1",
+    "url": "/sample-gator/static/js/main.f4e85b21.chunk.js"
+  },
+  {
+    "revision": "1759d2aca5a9006666be",
+    "url": "/sample-gator/static/js/runtime-main.a935f8fe.js"
+  }
+]);
